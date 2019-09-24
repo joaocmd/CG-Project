88261dd9cdffb8e2086670bf337cf35112a662fe
João David <joaodavid1999@hotmail.com>
2019-09-24 18:09:38 +0100

    More robot modelling
    Currently not rendering, invalid blending.

--- js/robot.js
+++ js/robot.js
@@ -1,11 +1,9 @@
-
 class Robot {
     constructor(x, y, z) {
         this.object = new THREE.Object3D();
         this.material = new THREE.MeshBasicMaterial({color: 0x777777, wireframe: true});
 
         var addWheel = (function(obj, mat, x, y, z) {
-            'use strict';
             let geometry = new THREE.SphereGeometry(4, 10, 10);
             let mesh = new THREE.Mesh(geometry, mat);
 
@@ -19,7 +17,7 @@ class Robot {
             addWheel(obj, mat, 21, 4, -21);
             addWheel(obj, mat, 21, 4, 21);
 
-            let baseGeometry = new THREE.CubeGeometry(50, 4, 50);
+            let baseGeometry = new THREE.BoxGeometry(50, 4, 50);
             let baseMesh = new THREE.Mesh(baseGeometry, mat);
             baseMesh.position.set(0, 10, 0);
             obj.add(baseMesh);
@@ -31,9 +29,9 @@ class Robot {
         });
 
         var newArm = (function(mat, x, y, z) {
-            let armObject = new THREE.Object3D();
+            let armObject = new THREE.Group();
 
-            let armGeometry = new THREE.CubeGeometry(5, 50, 5);
+            let armGeometry = new THREE.BoxGeometry(5, 50, 5);
             let armMesh = new THREE.Mesh(armGeometry, mat);
             armMesh.position.set(0, 25, 0);
             armObject.add(armMesh);
@@ -46,11 +44,37 @@ class Robot {
             armObject.position.set(x, y, z);
             return armObject;
         });
+
+        var addFinger = (function(parent, mat, x, y, z) {
+            let geometry = new THREE.BoxGeometry(2.5, 10, 2.5);
+            let mesh = new THREE.Mesh(geometry, mat);
+
+            mesh.position.set(x, y, z);
+            parent.add(mesh);
+        });
+
+        var newHand = (function(mat, x, y, z) {
+            let handObject = new THREE.Group();
+
+            addFinger(handObject, 3.75, 2, 0);
+            addFinger(handObject, -3.75, 2, 0);
+
+            let geometry = new THREE.CylinderGeometry(5, 5, 2, 10);
+            let mesh = new THREE.Mesh(geometry, mat);
+
+            handObject.add(mesh);
+        });
+
         addBase(this.object, this.material);
+
         this.arm = newArm(this.object, this.material, 0, 12, 0);
+        let forearm = newArm(this.object, this.material, 0, 50, 0);
         this.object.add(this.arm);
-        // let forearm = newArm(this.object, this.material, 0, 62, 0);
-        // forearm.rotation.z = -Math.PI/2;
+        this.arm.add(forearm);
+        forearm.rotation.z = -Math.PI/2; //Set 90º fixed angle
+
+        newHand(forearm, 0, 54, 0);
+
         this.object.position.set(x, y, z);
     }
 
