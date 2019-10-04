4562550658b2a1a4c62e06d5a6f0b78b39a5cebf
DMaxter <daniel_matos@outlook.pt>
2019-10-04 16:12:53 +0100

    Framerate velocity

--- js/robot.js
+++ js/robot.js
@@ -2,8 +2,8 @@
 class Robot {
 	constructor(x, y, z) {
 		this.object = new THREE.Group();
-		this.rotSpeed = 0.01
-		this.walkSpeed = 0.6;
+		this.rotSpeed = Math.PI / 8;
+		this.walkSpeed = 25;
 
 		let darkMat = new THREE.MeshBasicMaterial({color: 0x777777});
 		let lightMat = new THREE.MeshBasicMaterial({color: 0xbbbbbb});
@@ -116,18 +116,18 @@ class Robot {
 	update() {
 		let armRot = new THREE.Vector3(this.arm.rotation.x, this.armBase.rotation.y, 0);
 		if (input_getKey("Q"))  {
-			armRot.x += this.rotSpeed;
+			armRot.x += this.rotSpeed * time_deltaTime;
 		}
 		if (input_getKey("W")) {
-			armRot.x -= this.rotSpeed;
+			armRot.x -= this.rotSpeed * time_deltaTime;
 		}
 		armRot.x = Math.min(Math.max(armRot.x, -Math.PI/5), Math.PI/3);
 
 		if (input_getKey("A")) {
-			armRot.y += this.rotSpeed;
+			armRot.y += this.rotSpeed * time_deltaTime;
 		}
 		if (input_getKey("S")) {
-			armRot.y -= this.rotSpeed;
+			armRot.y -= this.rotSpeed * time_deltaTime;
 		}
 		this.arm.rotation.x = armRot.x;
 		this.armBase.rotation.y = armRot.y;
@@ -147,12 +147,12 @@ class Robot {
 		}
 		//Normalize vector magnitude for constant velocity and apply speed
 		if(velocity.length() !== 0){
-			velocity.multiplyScalar(1/velocity.length() * this.walkSpeed);	
-			velocity.multiplyScalar(1/velocity.length() * this.walkSpeed);	
-		}	
+			velocity.multiplyScalar(1/velocity.length() * this.walkSpeed * time_deltaTime);
+			velocity.multiplyScalar(1/velocity.length() * this.walkSpeed * time_deltaTime);
+		}
 
-		this.object.position.z += velocity.z;	
-		this.object.position.x += velocity.x;	
+		this.object.position.z += velocity.z;
+		this.object.position.x += velocity.x;
 	}
 
 	getObject3D() {
