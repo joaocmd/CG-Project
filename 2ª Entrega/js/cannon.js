e3eeacd8e2e34d7765f6083f91a7f80812038248
DMaxter <daniel_matos@outlook.pt>
2019-10-19 20:10:34 +0100

    No more balls swapping

--- 2ª Entrega/js/cannon.js
+++ 2ª Entrega/js/cannon.js
@@ -65,7 +65,7 @@ class Cannon {
 		ball.getObject3D().position.copy(this.object.position).add(direction.clone().multiplyScalar(180));
 		ball.getObject3D().rotation.copy(this.object.rotation);
 
-		ball.setVelocityVector(direction.multiplyScalar(randFloat(400, 2000)));
+		ball.setVelocityVector(direction.multiplyScalar(randFloat(400, 1200)));
 		scene.add(ball.object);
 		balls.push(ball);
 		objects.push(ball);
