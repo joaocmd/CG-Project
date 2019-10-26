b9a73da979caefe4a35c5b183be671b2cd7f04a2
João David <joaodavid1999@hotmail.com>
2019-10-26 19:33:52 +0100

    Prepare floor for new material workflow

--- 3ª Entrega/js/floor.js
+++ 3ª Entrega/js/floor.js
@@ -1,8 +1,10 @@
 class Floor {
   constructor(x, y, z) {
-    const width = 1350;
+    const width = 1500;
     const length = 2000;
     const wallHeight = 1000;
+
+    this.meshes = []
 
     this.object = new THREE.Group();
     let floorGeometry = new THREE.BoxGeometry(length, 5, width);
@@ -10,7 +12,7 @@ class Floor {
     this.floorLambertMaterial = new THREE.MeshLambertMaterial({color: 0x991717});
     
     this.floorMesh = new THREE.Mesh(floorGeometry, this.floorPhongMaterial);
-    this.object.add(this.floorMesh);
+    this.meshes.push(this.floorMesh);
 
     this.wallPhongMaterial = new THREE.MeshBasicMaterial({color: 0xe3e3e3});
     this.wallLambertMaterial = new THREE.MeshLambertMaterial({color: 0xe3e3e3});
@@ -18,26 +20,28 @@ class Floor {
 
     let wallGeometry = new THREE.BoxGeometry(length, 5, wallHeight);
     this.wallMesh1 = new THREE.Mesh(wallGeometry, this.wallPhongMaterial);
-    this.object.add(this.wallMesh1);
+    this.meshes.push(this.wallMesh1);
     this.wallMesh1.rotation.x = Math.PI/2;
     this.wallMesh1.position.z = -width/2;
     this.wallMesh1.position.y = wallHeight/2;
 
     wallGeometry = new THREE.BoxGeometry(wallHeight, 5, width);
     this.wallMesh2 = new THREE.Mesh(wallGeometry, this.wallPhongMaterial);
-    this.object.add(this.wallMesh2);
+    this.meshes.push(this.wallMesh2);
     this.wallMesh2.rotation.z = Math.PI/2;
     this.wallMesh2.position.x = length/2;
     this.wallMesh2.position.y = wallHeight/2;
 
     this.object.position.set(x, y - 30 - 2.5, z);
+
+    this.meshes.forEach(mesh => this.object.add(mesh));
     //this.updateMaterial();
   }
 
   updateMaterial () {
-    this.floorMesh.material = usePhong ? this.floorPhongMaterial : this.floorLambertMaterial;
-    this.wallMesh1.material = usePhong ? this.wallPhongMaterial : this.wallLambertMaterial;
-    this.wallMesh2.material = usePhong ? this.wallPhongMaterial : this.wallLambertMaterial;
+    this.meshes.forEach(mesh => {
+      mesh.geometry.groups[0].materialIndex = useMaterial;
+    });
   }
 
   getObject3D() {
