cd5fe157b7bed2da56f35d4f4be133b006afe1c1
DMaxter <daniel_matos@outlook.pt>
2019-11-04 16:38:20 +0000

    Corrections and shadows

--- 4ª Entrega/js/dice.js
+++ 4ª Entrega/js/dice.js
@@ -12,31 +12,31 @@ class Dice {
 			[
 				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice1.png")),
 											bumpMap: textureLoader.load(getTexture("dice1.png"))}),
-				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice2.png")),
-											bumpMap: textureLoader.load(getTexture("dice2.png"))}),
+				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice6.png")),
+											bumpMap: textureLoader.load(getTexture("dice6.png"))}),
 				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice3.png")),
 											bumpMap: textureLoader.load(getTexture("dice3.png"))}),
 				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice4.png")),
 											bumpMap: textureLoader.load(getTexture("dice4.png"))}),
 				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice5.png")),
 											bumpMap: textureLoader.load(getTexture("dice5.png"))}),
-				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice6.png")),
-											bumpMap: textureLoader.load(getTexture("dice6.png"))}),
+				new THREE.MeshPhongMaterial({map: textureLoader.load(getTexture("dice2.png")),
+											bumpMap: textureLoader.load(getTexture("dice2.png"))}),
 			],
 			[
 				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice1.png"))}),
-				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice2.png"))}),
+				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice6.png"))}),
 				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice3.png"))}),
 				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice4.png"))}),
 				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice5.png"))}),
-				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice6.png"))}),
+				new THREE.MeshBasicMaterial({map: textureLoader.load(getTexture("dice2.png"))}),
 			],
 		]
 
 
 		let geometry = new THREE.BoxGeometry(size, size, size, 4, 4, 4);
 		let mesh = new THREE.Mesh(geometry);
-		mesh.receiveShadow = true;
+		mesh.castShadow = true;
 		mesh.rotation.x = -Math.PI/2;
 
 		this.meshMaterials = new MeshMaterials(mesh, materials);
@@ -56,7 +56,7 @@ class Dice {
 	}
 
 	update() {
-		this.object.rotateOnWorldAxis(UP_VECTOR, this.spinSpeed * time_deltaTime);
+		this.object.rotateOnWorldAxis(UP_VECTOR, ROTATION_DICE * time_deltaTime);
 	}
 
 	getObject3D() {
